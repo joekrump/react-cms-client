9e38ea0d88681ab44f0eb32f25ec7c3bdc2b464f
Joseph Krump <krump.joseph@gmail.com>
2016-07-26 20:02:42 -0700

    added base Form component

---
+++ src/components/Form/Form.js
@@ -0,0 +1,22 @@
+// src/components/Form/Form.js
+import React, {PropTypes} from 'react';
+
+export default React.createClass({
+  displayName: 'Form',
+
+  propTypes: {
+    children: PropTypes.node,
+    values: PropTypes.object,
+    update: PropTypes.func,
+    reset: PropTypes.func,
+    onSubmit: PropTypes.func
+  },
+
+  render() {
+    return (
+      <form>
+        {this.props.children}
+      </form>
+    );
+  }
+});
