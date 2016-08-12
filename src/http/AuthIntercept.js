574505cfa170f5cd73ef5ea22239d8706b53089e
Joseph Krump <krump.joseph@gmail.com>
2016-08-11 19:32:19 -0700

    If HTTP request response is a 401 with token expired, then log out the user and redirect to /login

--- src/http/AuthIntercept.js
+++ src/http/AuthIntercept.js
@@ -1,8 +1,14 @@
 import { store } from '../store'
 
 const AuthIntercept = require('superagent-intercept')((err, res) => {
-	if(res.status === 401 && store.getState().auth.logged_in) {
-		console.log('logged in but 401');
+	// console.log(res.status);
+	// console.log(res);
+	if(res.status === 401 && store.getState().auth.logged_in && res.body.message === "Token has expired") {
+		// If user is logged in and their token has expired, log them out.
+		store.dispatch({
+			type: 'USER_LOGGED_OUT',
+			redirectPath: '/login'
+		})
 	} 
 });
 
