19a4122fc4371e09bc8121dfce370dc223f53c71
Joseph Krump <krump.joseph@gmail.com>
2016-08-17 12:48:36 -0700

    added example code with mutability for entities set

--- src/components/Editor/DraftEditor.js
+++ src/components/Editor/DraftEditor.js
@@ -1,38 +1,186 @@
 import React from 'react'
-import {Editor, EditorState, RichUtils} from 'draft-js';
+import {convertFromRaw,
+        convertToRaw,
+        CompositeDecorator,
+        ContentState,
+        Editor,
+        EditorState,
+        Entity} from 'draft-js';
 import {cyan50} from 'material-ui/styles/colors'
 import './DraftEditor.css';
+
+const rawContent = {
+  blocks: [
+    {
+      text: (
+        'This is an "immutable" entity: Superman. Deleting any ' +
+        'characters will delete the entire entity. Adding characters ' +
+        'will remove the entity from the range.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 31, length: 8, key: 'first'}],
+    },
+    {
+      text: '',
+      type: 'unstyled',
+    },
+    {
+      text: (
+        'This is a "mutable" entity: Batman. Characters may be added ' +
+        'and removed.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 28, length: 6, key: 'second'}],
+    },
+    {
+      text: '',
+      type: 'unstyled',
+    },
+    {
+      text: (
+        'This is a "segmented" entity: Green Lantern. Deleting any ' +
+        'characters will delete the current "segment" from the range. ' +
+        'Adding characters will remove the entire entity from the range.'
+      ),
+      type: 'unstyled',
+      entityRanges: [{offset: 30, length: 13, key: 'third'}],
+    },
+  ],
+
+  entityMap: {
+    first: {
+      type: 'TOKEN',
+      mutability: 'IMMUTABLE',
+    },
+    second: {
+      type: 'TOKEN',
+      mutability: 'MUTABLE',
+    },
+    third: {
+      type: 'TOKEN',
+      mutability: 'SEGMENTED',
+    },
+  },
+};
 
 class DraftEditor extends React.Component {
   constructor(props) {
     super(props);
-    this.state = {editorState: EditorState.createEmpty()};
+
+    const decorator = new CompositeDecorator([
+      {
+        strategy: getEntityStrategy('IMMUTABLE'),
+        component: TokenSpan,
+      },
+      {
+        strategy: getEntityStrategy('MUTABLE'),
+        component: TokenSpan,
+      },
+      {
+        strategy: getEntityStrategy('SEGMENTED'),
+        component: TokenSpan,
+      },
+    ]);
+
+    const blocks = convertFromRaw(rawContent);
+
+    this.state = {
+      editorState: EditorState.createWithContent(blocks, decorator),
+    };
+
+    this.focus = () => this.refs.editor.focus();
     this.onChange = (editorState) => this.setState({editorState});
-    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+    this.logState = () => {
+      const content = this.state.editorState.getCurrentContent();
+      console.log(convertToRaw(content));
+    };
   }
-  _onBoldClick() {
-    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
-  }
-  handleKeyCommand(command) {
-    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
-    if (newState) {
-      this.onChange(newState);
-      return true;
-    }
-    return false;
-  }
+
   render() {
     return (
-      <div>
-        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
-        <Editor
-          editorState={this.state.editorState}
-          handleKeyCommand={this.handleKeyCommand}
-          onChange={this.onChange}
+      <div style={styles.root}>
+        <div style={styles.editor} onClick={this.focus}>
+          <Editor
+            editorState={this.state.editorState}
+            onChange={this.onChange}
+            placeholder="Enter some text..."
+            ref="editor"
+          />
+        </div>
+        <input
+          onClick={this.logState}
+          style={styles.button}
+          type="button"
+          value="Log State"
         />
       </div>
-    )
+    );
   }
 }
+
+function getEntityStrategy(mutability) {
+  return function(contentBlock, callback) {
+    contentBlock.findEntityRanges(
+      (character) => {
+        const entityKey = character.getEntity();
+        if (entityKey === null) {
+          return false;
+        }
+        return Entity.get(entityKey).getMutability() === mutability;
+      },
+      callback
+    );
+  };
+}
+
+function getDecoratedStyle(mutability) {
+  switch (mutability) {
+    case 'IMMUTABLE': return styles.immutable;
+    case 'MUTABLE': return styles.mutable;
+    case 'SEGMENTED': return styles.segmented;
+    default: return null;
+  }
+}
+
+const TokenSpan = (props) => {
+  const style = getDecoratedStyle(
+    Entity.get(props.entityKey).getMutability()
+  );
+  return (
+    <span {...props} style={style}>
+      {props.children}
+    </span>
+  );
+};
+
+const styles = {
+  root: {
+    fontFamily: '\'Helvetica\', sans-serif',
+    padding: 20,
+    width: 600,
+  },
+  editor: {
+    border: '1px solid #ccc',
+    cursor: 'text',
+    minHeight: 80,
+    padding: 10,
+  },
+  button: {
+    marginTop: 10,
+    textAlign: 'center',
+  },
+  immutable: {
+    backgroundColor: 'rgba(0, 0, 0, 0.2)',
+    padding: '2px 0',
+  },
+  mutable: {
+    backgroundColor: 'rgba(204, 204, 255, 1.0)',
+    padding: '2px 0',
+  },
+  segmented: {
+    backgroundColor: 'rgba(248, 222, 126, 1.0)',
+    padding: '2px 0',
+  },
+};
 
 export default DraftEditor;
