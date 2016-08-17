7fd6c9346b795ff94e7cfe605fbaa7c1620107b9
Joseph Krump <krump.joseph@gmail.com>
2016-08-16 17:23:59 -0700

    installed draft-js-editor module and began building basic editor component

---
+++ src/components/Editor/DraftEditor.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Editor from 'draft-js-editor'
+
+const DraftEditor = () => {
+  render() {
+    return (
+      <div>
+        A sample text editor
+        <Editor />
+      </div>
+    ) 
+  }
+}
