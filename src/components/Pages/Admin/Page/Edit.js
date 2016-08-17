81e47c53fdc210a7fb5d26cc092aa0f5a200da3f
Joseph Krump <krump.joseph@gmail.com>
2016-08-16 18:16:45 -0700

    draft-js-editor somewhat working (RECOMMEND SWITCHING OUT)

--- src/components/Pages/Admin/Page/Edit.js
+++ src/components/Pages/Admin/Page/Edit.js
@@ -1,11 +1,12 @@
 import React from 'react'
-
+import DraftEditor from '../../../Editor/DraftEditor'
 const Edit = () => ({
 
 	render() {
 		return (
 			<div>
 				<h1>Edit a page</h1>
+        <DraftEditor />
 			</div>
 		)
 		
