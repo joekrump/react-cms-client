559c9ab6b9c85cf929618ea53407bf488b9c3047
Joseph Krump <krump.joseph@gmail.com>
2016-08-17 12:34:54 -0700

    added bold button and handling key events to editor while following draft.js tutorial

--- src/components/Editor/DraftEditor.js
+++ src/components/Editor/DraftEditor.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import {Editor, EditorState} from 'draft-js';
+import {Editor, EditorState, RichUtils} from 'draft-js';
 import {cyan50} from 'material-ui/styles/colors'
 import './DraftEditor.css';
 
@@ -8,10 +8,30 @@ class DraftEditor extends React.Component {
     super(props);
     this.state = {editorState: EditorState.createEmpty()};
     this.onChange = (editorState) => this.setState({editorState});
+    this.handleKeyCommand = this.handleKeyCommand.bind(this);
+  }
+  _onBoldClick() {
+    this.onChange(RichUtils.toggleInlineStyle(this.state.editorState, 'BOLD'));
+  }
+  handleKeyCommand(command) {
+    const newState = RichUtils.handleKeyCommand(this.state.editorState, command);
+    if (newState) {
+      this.onChange(newState);
+      return true;
+    }
+    return false;
   }
   render() {
-    const {editorState} = this.state;
-    return <Editor editorState={editorState} onChange={this.onChange} />;
+    return (
+      <div>
+        <button onClick={this._onBoldClick.bind(this)}>Bold</button>
+        <Editor
+          editorState={this.state.editorState}
+          handleKeyCommand={this.handleKeyCommand}
+          onChange={this.onChange}
+        />
+      </div>
+    )
   }
 }
 
