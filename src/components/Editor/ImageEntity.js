a917337e768a91287318e896757ad21d809d4726
Joseph Krump <krump.joseph@gmail.com>
2016-08-18 20:32:40 -0700

    added resizable components

--- src/components/Editor/ImageEntity.js
+++ src/components/Editor/ImageEntity.js
@@ -1,7 +1,29 @@
 import React from 'react';
+import Resizable from '../Resizable/Resizable';
+import ResizableBox from '../Resizable/ResizableBox';
 
-const ImageEntity = (props) => {
-  return <img src={props.src} style={props.style} />;
-};
+
+import './css/Resizable.css';
+
+class ImageEntity extends React.Component {
+
+  state = {width: 200, height: 200};
+
+  onClick = () => {
+    this.setState({width: 200, height: 200});
+  };
+
+  onResize = (event, {element, size}) => {
+    this.setState({width: size.width, height: size.height});
+  };
+
+  render() {
+    return (
+      <ResizableBox className="box" height={600} width={600} lockAspectRatio={true} onResize={this.onResize}>
+        <img src={this.props.src} style={{...this.props.style, 'padding': '10px'}} />
+      </ResizableBox>
+    )
+  }
+}
 
 export default ImageEntity;
---
+++ src/components/Resizable/ResizableBox.js
@@ -0,0 +1,57 @@
+// @flow
+import {default as React, PropTypes} from 'react';
+import Resizable from './Resizable';
+
+type State = {width: number, height: number, aspectRatio: number};
+type Size = {width: number, height: number};
+type ResizeData = {element: Element, size: Size};
+
+// An example use of Resizable.
+export default class ResizableBox extends React.Component {
+  static propTypes = {
+    height: PropTypes.number,
+    width: PropTypes.number
+  };
+
+  static defaultProps = {
+    handleSize: [20,20]
+  };
+
+  state: State = {
+    width: this.props.width,
+    height: this.props.height,
+  };
+
+  onResize = (event, {element, size}) => {
+    const {width, height} = size;
+
+    this.setState(size, () => {
+      this.props.onResize && this.props.onResize(event, {element, size});
+    });
+  };
+  onResize: (event: Event, data: ResizeData) => void;
+
+  render(): React.Element {
+    // Basic wrapper around a Resizable instance.
+    // If you use Resizable directly, you are responsible for updating the child component
+    // with a new width and height.
+    const {handleSize, onResizeStart, onResizeStop, draggableOpts,
+         minConstraints, maxConstraints, lockAspectRatio, width, height, ...props} = this.props;
+    return (
+      <Resizable
+        handleSize={handleSize}
+        width={this.state.width}
+        height={this.state.height}
+        onResizeStart={onResizeStart}
+        onResize={this.onResize}
+        onResizeStop={onResizeStop}
+        draggableOpts={draggableOpts}
+        minConstraints={minConstraints}
+        maxConstraints={maxConstraints}
+        lockAspectRatio={lockAspectRatio}
+        >
+        <div style={{width: this.state.width + 'px', height: this.state.height + 'px'}} {...props} />
+      </Resizable>
+    );
+  }
+}
