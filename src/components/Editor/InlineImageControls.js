ebcd7cd389a959472c343eb3d279b9d89764212b
Joseph Krump <krump.joseph@gmail.com>
2016-08-23 00:01:12 -0700

    added tooltips for alignment buttons

--- src/components/Editor/InlineImageControls.js
+++ src/components/Editor/InlineImageControls.js
@@ -10,13 +10,22 @@ const InlineImageControls = (props) => {
   return (
     <div className="image-editor-control"  style={{position: 'absolute', width: '100%', top: 0, right: 0}}>
       <div style={{position: 'relative', width: 200, margin: '0 auto', top: 10, right: 0, zIndex: 40, backgroundColor: 'rgba(0,0,0,0.7)', color: '#fff', padding: 10, textAlign: 'center'}}>
-        <IconButton onTouchTap={props.handleAlignLeft}>
+        <IconButton onTouchTap={props.handleAlignLeft}
+          tooltip="Align Left"
+          tooltipPosition="top-center"
+        >
           <FormatAlignLeft />
         </IconButton>
-        <IconButton onTouchTap={props.handleAlignCenter}>
+        <IconButton onTouchTap={props.handleAlignCenter}
+          tooltip="Center"
+          tooltipPosition="top-center"
+        >
           <FormatAlignCenter />
         </IconButton>
-        <IconButton onTouchTap={props.handleAlignRight}>
+        <IconButton onTouchTap={props.handleAlignRight}
+          tooltip="Align Right"
+          tooltipPosition="top-center"
+        >
           <FormatAlignRight />
         </IconButton>
       </div>
