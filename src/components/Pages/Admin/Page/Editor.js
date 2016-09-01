461fb76bc8048a7a6df4e7c54b0bbf0bdd567ebf
Joseph Krump <krump.joseph@gmail.com>
2016-08-31 18:48:16 -0700

    removed commented-out code in editor

--- src/components/Pages/Admin/Page/Editor.js
+++ src/components/Pages/Admin/Page/Editor.js
@@ -1,19 +1,6 @@
 import React from 'react'
-
 import Quill from 'quill'
  
-/*
-pm.setOption("tooltipMenu", {
-  selectedBlockMenu: true,
-  inlineContent: [inlineGroup,insertMenu],
-  blockContent: [[blockGroup, textblockMenu,alignGroup], [contentInsertMenu, questionInsertMenu]],
-})*/
-  
-
-// import prosemirror from "prosemirror"
-// import BasicSchema from "prosemirror/dist/schema-basic"
-// import "prosemirror/dist/menu/toolt`ipmenu"
-// import './quill.core.css'
 import './quill.bubble.css'
 
 const Editor = () => ({
@@ -38,18 +25,6 @@ const Editor = () => ({
     };
 
     var editor = new Quill('#editor-root', options);
-
-    // pm.setOption("tooltipMenu", {
-    //   selectedBlockMenu: true,
-    //   inlineContent: [inlineGroup,insertMenu],
-    //   blockContent: [[blockGroup, textblockMenu,alignGroup], [contentInsertMenu, questionInsertMenu]],
-    // })
-
-    // var editor = new prosemirror.ProseMirror({
-    //   place: document.getElementById('editor-root'),
-    //   schema: BasicSchema.schema,
-    //   doc: BasicSchema.schema.parseDOM(contentWrapper)
-    // })
   },
   render() {
     return (
