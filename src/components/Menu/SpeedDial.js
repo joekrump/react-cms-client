04f357b8ce61197ccbfecc2763c84375d3d948c3
Joseph Krump <krump.joseph@gmail.com>
2016-09-01 01:09:45 -0700

    new page button added to SpeedDial

--- src/components/Menu/SpeedDial.js
+++ src/components/Menu/SpeedDial.js
@@ -4,6 +4,7 @@ import {connect} from 'react-redux'
 import FloatingActionButton from 'material-ui/FloatingActionButton'
 
 import AddIcon from 'material-ui/svg-icons/content/add'
+import AddPageIcon from 'material-ui/svg-icons/action/note-add'
 import UserIcon from 'material-ui/svg-icons/social/person-add'
 import BookIcon from 'material-ui/svg-icons/av/library-books'
 import RoleIcon from 'material-ui/svg-icons/social/group-add'
@@ -15,6 +16,7 @@ import { push } from 'react-router-redux'
 import './SpeedDial.css'
 
 const actions = [
+  {icon: <AddPageIcon />, route: '/admin/pages/new', tooltipText: 'Create a new Page'},
   {icon: <UserIcon />, route: '/admin/users/new', tooltipText: 'Create a new User'},
   {icon: <BookIcon />, route: '/admin/books/new', tooltipText: 'Create a new Book'},
   {icon: <RoleIcon />, route: '/admin/roles/new', tooltipText: 'Create a new User Role'},
