4d76640c886e36142276ae94d126bd77ef22c00e
Joseph Krump <krump.joseph@gmail.com>
2016-09-03 16:28:59 -0700

    fixed some indenting in ImageUploader

--- src/components/Editor/ImageUploader.js
+++ src/components/Editor/ImageUploader.js
@@ -160,109 +160,109 @@ class ImageUploader {
   }
 
   buildCloudinaryURL(filename, transforms) {
-      // Build a Cloudinary URL from a filename and the list of transforms 
-      // supplied. Transforms should be specified as objects (e.g {a: 90} becomes
-      // 'a_90').
-      var i, name, transform, transformArgs, transformPaths, urlParts;
+    // Build a Cloudinary URL from a filename and the list of transforms 
+    // supplied. Transforms should be specified as objects (e.g {a: 90} becomes
+    // 'a_90').
+    var i, name, transform, transformArgs, transformPaths, urlParts;
 
-      // Convert the transforms to paths
-      transformPaths = [];
-      for  (i = 0; i < transforms.length; i++) {
-        transform = transforms[i];
+    // Convert the transforms to paths
+    transformPaths = [];
+    for  (i = 0; i < transforms.length; i++) {
+      transform = transforms[i];
 
-          // Convert each of the object properties to a transform argument
-          transformArgs = [];
-          for (name in transform) {
-            if (transform.hasOwnProperty(name)) {
-              transformArgs.push(name + '_' + transform[name]);
-            }
+        // Convert each of the object properties to a transform argument
+        transformArgs = [];
+        for (name in transform) {
+          if (transform.hasOwnProperty(name)) {
+            transformArgs.push(name + '_' + transform[name]);
           }
-          
-          transformPaths.push(transformArgs.join(','));
         }
-
-      // Build the URL
-      urlParts = [CLOUDINARY_RETRIEVE_URL];
-      if (transformPaths.length > 0) {
-        urlParts.push(transformPaths.join('/'));
+        
+        transformPaths.push(transformArgs.join(','));
       }
-      urlParts.push(filename);
 
-      return urlParts.join('/');
+    // Build the URL
+    urlParts = [CLOUDINARY_RETRIEVE_URL];
+    if (transformPaths.length > 0) {
+      urlParts.push(transformPaths.join('/'));
     }
+    urlParts.push(filename);
 
-    parseCloudinaryURL(url) {
-      // Parse a Cloudinary URL and return the filename and list of transforms
-      var filename, i, j, transform, transformArgs, transforms, urlParts;
-
-      // Strip the URL down to just the transforms, version (optional) and
-      // filename.
-      url = url.replace(CLOUDINARY_RETRIEVE_URL, '');
-
-      // Split the remaining path into parts
-      urlParts = url.split('/');
-
-      // The path starts with a '/' so the first part will be empty and can be
-      // discarded.
-      urlParts.shift();
-
-      // Extract the filename
-      filename = urlParts.pop();
-
-      // Strip any version number from the URL
-      if (urlParts.length > 0 && urlParts[urlParts.length - 1].match(/v\d+/)) {
-        urlParts.pop();
-      }
-
-      // Convert the remaining parts into transforms (e.g `w_90,h_90,c_fit >
-      // {w: 90, h: 90, c: 'fit'}`).
-      transforms = [];
-      for (i = 0; i < urlParts.length; i++) {
-        transformArgs = urlParts[i].split(',');
-        transform = {};
-        for (j = 0; j < transformArgs.length; j++) {
-          transform[transformArgs[j].split('_')[0]] =
-          transformArgs[j].split('_')[1];
-        }
-        transforms.push(transform);
-      }
-
-      return [filename, transforms];
-    }
-
-    rotate(angle) {
-      // Handle a request by the user to rotate the image
-      var height, transforms, width;
-      
-      // Update the angle of the image
-      image.angle += angle;
-
-      // Stay within 0-360 degree range
-      if (image.angle < 0) {
-        image.angle += 360;
-      } else if (image.angle > 270) {
-        image.angle -= 360;
-      }
-
-      // Rotate the image's dimensions
-      width = image.width;
-      height = image.height;
-      image.width = height;
-      image.height = width;
-      image.maxWidth = width;
-      
-      // Build the transform to rotate the image
-      transforms = [{c: 'fit', h: 600, w: 600}];
-      if (image.angle > 0) {
-        transforms.unshift({a: image.angle});
-      }
-
-      // Build a URL for the transformed image
-      image.url = this.buildCloudinaryURL(image.filename, transforms);
-      
-      // Update the image in the dialog
-      dialog.populate(image.url, [image.width, image.height]);
-    }
+    return urlParts.join('/');
   }
+
+  parseCloudinaryURL(url) {
+    // Parse a Cloudinary URL and return the filename and list of transforms
+    var filename, i, j, transform, transformArgs, transforms, urlParts;
+
+    // Strip the URL down to just the transforms, version (optional) and
+    // filename.
+    url = url.replace(CLOUDINARY_RETRIEVE_URL, '');
+
+    // Split the remaining path into parts
+    urlParts = url.split('/');
+
+    // The path starts with a '/' so the first part will be empty and can be
+    // discarded.
+    urlParts.shift();
+
+    // Extract the filename
+    filename = urlParts.pop();
+
+    // Strip any version number from the URL
+    if (urlParts.length > 0 && urlParts[urlParts.length - 1].match(/v\d+/)) {
+      urlParts.pop();
+    }
+
+    // Convert the remaining parts into transforms (e.g `w_90,h_90,c_fit >
+    // {w: 90, h: 90, c: 'fit'}`).
+    transforms = [];
+    for (i = 0; i < urlParts.length; i++) {
+      transformArgs = urlParts[i].split(',');
+      transform = {};
+      for (j = 0; j < transformArgs.length; j++) {
+        transform[transformArgs[j].split('_')[0]] =
+        transformArgs[j].split('_')[1];
+      }
+      transforms.push(transform);
+    }
+
+    return [filename, transforms];
+  }
+
+  rotate(angle) {
+    // Handle a request by the user to rotate the image
+    var height, transforms, width;
+    
+    // Update the angle of the image
+    image.angle += angle;
+
+    // Stay within 0-360 degree range
+    if (image.angle < 0) {
+      image.angle += 360;
+    } else if (image.angle > 270) {
+      image.angle -= 360;
+    }
+
+    // Rotate the image's dimensions
+    width = image.width;
+    height = image.height;
+    image.width = height;
+    image.height = width;
+    image.maxWidth = width;
+    
+    // Build the transform to rotate the image
+    transforms = [{c: 'fit', h: 600, w: 600}];
+    if (image.angle > 0) {
+      transforms.unshift({a: image.angle});
+    }
+
+    // Build a URL for the transformed image
+    image.url = this.buildCloudinaryURL(image.filename, transforms);
+    
+    // Update the image in the dialog
+    dialog.populate(image.url, [image.width, image.height]);
+  }
+}
 
 export default ImageUploader
