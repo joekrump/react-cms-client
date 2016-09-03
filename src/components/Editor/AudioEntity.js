b9f8d30d8348ae680c1bb494a0b922cc029bda34
Joseph Krump <krump.joseph@gmail.com>
2016-09-02 17:46:58 -0700

    removed editor components that are no longer in use

--- src/components/Editor/AudioEntity.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const AudioEntity = (props) => {
-  return <audio controls src={props.src} style={props.style} />;
-};
-
-export default AudioEntity;
--- src/components/Editor/VideoEntity.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const VideoEntity = (props) => {
-  return <video controls src={props.src} style={props.style} />;
-};
-
-export default VideoEntity;
