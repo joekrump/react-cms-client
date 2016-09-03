2790f013e8da7c1f2dba4aa7161c9d35e9aa1077
Joseph Krump <krump.joseph@gmail.com>
2016-09-02 17:41:05 -0700

    basic content tools editor loading in conents

--- src/helpers/ScriptsHelper.js
+++ src/helpers/ScriptsHelper.js
@@ -1,12 +1,28 @@
-export function loadScript(filename){
+export function loadScript(filepath, onLoadCallback){
     var fileref=document.createElement('script')
+    
+    if(onLoadCallback) {
+        fileref.onload = function() {
+            onLoadCallback();
+        }
+    }
+    
     fileref.setAttribute("type","text/javascript")
-    fileref.setAttribute("src", filename)
+    fileref.setAttribute("src", filepath)
+    document.getElementsByTagName("head")[0].appendChild(fileref)
 }
 
-export function loadStylesheet(filepath) {
+export function loadStylesheet(filepath, onLoadCallback) {
     var fileref=document.createElement("link")
+
+    if(onLoadCallback) {
+        fileref.onload = function() {
+            onLoadCallback();
+        }
+    }
+    
     fileref.setAttribute("rel", "stylesheet")
     fileref.setAttribute("type", "text/css")
-    fileref.setAttribute("href", filename)
+    fileref.setAttribute("href", filepath)
+    document.getElementsByTagName("head")[0].appendChild(fileref)
 }
