9308ee5d67b807b6f2d567a4504f29e2584d8c03
Joseph Krump <krump.joseph@gmail.com>
2016-09-12 12:00:28 -0700

    admin Index updated to ES6 class

--- src/components/Pages/Admin/Resources/Index/Index.js
+++ src/components/Pages/Admin/Resources/Index/Index.js
@@ -8,13 +8,14 @@ import AdminLayout from '../../Layout/Layout'
 import { capitalize } from '../../../../../helpers/StringHelper'
 import { apiGet, updateToken } from '../../../../../http/requests'
 
-const Index = React.createClass({
-  getInitialState() {
-    return {
+class Index extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       items: [],
       loading: true
     }
-  },
+  }
   setItems(resourceNamePlural){
     this.setState({loading: true})
     apiGet(resourceNamePlural)
@@ -28,18 +29,17 @@ const Index = React.createClass({
           updateToken(res.header.authorization)
           this.setState({items: res.body.data})
         }
-
       }.bind(this))
-  },
+  }
   componentDidMount() {
     this.setItems(this.props.params.resourceNamePlural.toLowerCase());
-  },
+  }
   componentWillReceiveProps(nextProps){
     // TODO: update how this works by tying into redux
     if(nextProps.params.resourceNamePlural !== this.props.params.resourceNamePlural) {
       this.setItems(nextProps.params.resourceNamePlural);
     }
-  },
+  }
   render() {
     let items = null;
 
@@ -68,5 +68,6 @@ const Index = React.createClass({
       </AdminLayout>
     );
   }
-});
+}
+
 export default Index;
