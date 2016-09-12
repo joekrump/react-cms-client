a5b56441fd0a1d9c6d587eb481c74151b953f418
Joseph Krump <krump.joseph@gmail.com>
2016-09-12 12:03:23 -0700

    IndexItem to ES6 class

--- src/components/Pages/Admin/Resources/Index/IndexItem.js
+++ src/components/Pages/Admin/Resources/Index/IndexItem.js
@@ -8,35 +8,37 @@ import IndexItemActions from './IndexItemActions'
 import { VelocityComponent } from 'velocity-react';
 import 'velocity-animate/velocity.ui';
 
-const IndexItem = React.createClass({
-
-  getInitialState(){
-    return {
+class IndexItem extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
       visible: true
     }
-  },
+  }
+
   showItem(visible = false) {
     this.setState({visible});
-  },
+  }
+  
   getText(){
     return(
       <div style={{color: muiTheme.palette.textColor}}><strong>{this.props.primary}</strong>{this.props.secondary ? (<span> -&nbsp;{this.props.secondary}</span>) : null}</div>
     )
-  },
-  render(){
+  }
 
+  render(){
     return(
       <VelocityComponent style={{display: 'block'}} animation={{height: this.state.visible ? 50 : 0, opacity: this.state.visible ? 1 : 0}} duration={300}>
         <ListItem
           className="index-list-item"
           disabled
-          rightIconButton={<IndexItemActions resourceType={this.props.resourceType} id={this.props.id} deleteCallback={ this.showItem }/>}
+          rightIconButton={<IndexItemActions resourceType={this.props.resourceType} id={this.props.id} deleteCallback={ () => this.showItem() }/>}
           primaryText={this.getText()}
           style={{backgroundColor: fade(fullBlack, 0.7)}}
         />
       </VelocityComponent>
     );
   }
-})
+}
 
 export default IndexItem;
