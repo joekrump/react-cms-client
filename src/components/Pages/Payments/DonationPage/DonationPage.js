8612d8ad6f78f0b9421c86bc7c9aa4ef77eacba1
Joseph Krump <krump.joseph@gmail.com>
2016-09-12 12:09:18 -0700

    DonationPage to pure function

--- src/components/Pages/Payments/DonationPage/DonationPage.js
+++ src/components/Pages/Payments/DonationPage/DonationPage.js
@@ -5,33 +5,28 @@ import PaymentContent from '../Partials/PaymentContent';
 import { connect } from 'react-redux';
 import { grey100 } from 'material-ui/styles/colors';
 
-const DonationPage = React.createClass({
-  handleSnackbarClose(){
-    this.props.toggleSnackBar(false);
-  },
-  render() {
-    return(<div>
-      <h1>Donation Page</h1>
-      <Paper zDepth={2} className="form-container">
-        <PaymentContent />
-        <Snackbar
-          open={this.props.snackbarOpen}
-          message={
-            (<div style={{color: this.props.snackbarColor}}>
-              <h2 style={{margin: '0'}}>{this.props.snackbarHeaderText}</h2>
-              {this.props.snackbarMessage}
-            </div>)}
-          autoHideDuration={4000}
-          bodyStyle={{backgroundColor: grey100, height: 'auto'}}
-          style={{
-            transform: this.props.snackbarOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, 200px, 0)'
-          }}
-          onRequestClose={this.handleSnackbarClose}
-        />
-      </Paper>
-    </div>);
-  }
-})
+const DonationPage = (props) => (
+  <div>
+    <h1>Donation Page</h1>
+    <Paper zDepth={2} className="form-container">
+      <PaymentContent />
+      <Snackbar
+        open={props.snackbarOpen}
+        message={
+          (<div style={{color: props.snackbarColor}}>
+            <h2 style={{margin: '0'}}>{props.snackbarHeaderText}</h2>
+            {props.snackbarMessage}
+          </div>)}
+        autoHideDuration={4000}
+        bodyStyle={{backgroundColor: grey100, height: 'auto'}}
+        style={{
+          transform: props.snackbarOpen ? 'translate3d(0, 0, 0)' : 'translate3d(0, 200px, 0)'
+        }}
+        onRequestClose={() => props.toggleSnackBar(false)}
+      />
+    </Paper>
+  </div>
+);
 
 const mapStateToProps = (state) => {
   return {
