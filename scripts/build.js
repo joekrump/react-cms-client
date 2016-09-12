2e4722e1b94472f54e5e1b41b5d5a28058791a49
Joseph Krump <krump.joseph@gmail.com>
2016-09-12 14:30:23 -0700

    adding webpack config so that when `npm run build` is run, it will compile the server also.

--- scripts/build.js
+++ scripts/build.js
@@ -3,18 +3,30 @@ process.env.NODE_ENV = 'production';
 var path = require('path');
 var rimrafSync = require('rimraf').sync;
 var webpack = require('webpack');
+
 var config = require('../config/webpack.config.prod');
+var serverconfig = require('../config/webpack.config.server');
 
 var isInNodeModules = 'node_modules' ===
   path.basename(path.resolve(path.join(__dirname, '..', '..')));
 var relative = isInNodeModules ? '../..' : '.';
 rimrafSync(relative + '/build');
 
+webpack(serverconfig).run(function(err, stats) {
+  if (err) {
+    console.error('Failed to create a server build. Reason:');
+    console.error(err.message || err);
+    process.exit(1);
+  }
+
+  console.log('Run Server with:    npm run server');
+});
+
 webpack(config).run(function(err, stats) {
   if (err) {
     console.error('Failed to create a production build. Reason:');
     console.error(err.message || err);
-    process.exit(1);
+    process.exit(2);
   }
 
   var openCommand = process.platform === 'win32' ? 'start' : 'open';
