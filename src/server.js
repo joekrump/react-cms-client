ac3ca79f785a211ebb5d8be001fe678ca7fb1e6a
Joseph Krump <krump.joseph@gmail.com>
2016-09-14 22:56:43 -0700

    server rendering working (without background image)

--- src/server.js
+++ src/server.js
@@ -62,28 +62,25 @@ function configureStore(memoryHistory, initialState) {
     routing: routerReducer
   }
 
-  const store = makeStore(reducers, middleware)
-
-  return store;
+  return makeStore(reducers, middleware, () => sagaMiddleware.run(rootSaga));
 }
+
+
 
 app.get('*',(req, res) => {
 
   const memoryHistory = createMemoryHistory(req.path)
-  let store = configureStore(memoryHistory)
+  const store         = configureStore(memoryHistory)
+  syncHistoryWithStore(memoryHistory, store)
+  const routes        = getRoutes(store);
 
-  sagaMiddleware.run(rootSaga)
-
-  const history = syncHistoryWithStore(memoryHistory, store)
-
-  match({ getRoutes(store), location: req.url }, (error, redirectLocation, renderProps) => {
+  match({routes , location: req.url }, (error, redirectLocation, renderProps) => {
         
     if (error) {
       res.status(500).send(error.message)
     } else if (redirectLocation) {
       res.redirect(302, redirectLocation.pathname + redirectLocation.search)
     } else if (renderProps) {
-      console.log(renderProps);
       res.locals.reactMarkup = renderPage(renderProps);
       // Success!
       res.status(200).render('index.html')
