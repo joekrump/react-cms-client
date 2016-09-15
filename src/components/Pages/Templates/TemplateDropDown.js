ce853fc74ee49b0d7fe8118c035d1a298230bf42
Joseph Krump <krump.joseph@gmail.com>
2016-09-15 12:55:31 -0700

    Template dropdown menu added

---
+++ src/components/Pages/Templates/TemplateDropDown.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import DropDownMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+
+
+export default class TemplateDropDown extends React.Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      value: props.defaultValue ? props.defaultValue : 1
+    };
+  }
+
+  handleChange = (event, index, value) => {
+    this.setState({value});
+  }
+
+  render() {
+    let menuItems = this.props.templateOptions.map((option) => {
+      return (<MenuItem value={option.value} primaryText={option.text} />)
+    })
+    return (
+      <div>
+        <DropDownMenu value={this.state.value} onChange={this.handleChange}>
+          {menuItems}
+        </DropDownMenu>
+      </div>
+    );
+  }
+}
