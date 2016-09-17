2528dee227dc97ef4162744fe92cd02e55741624
Joseph Krump <krump.joseph@gmail.com>
2016-09-16 19:30:40 -0700

    disabled button styling added for RaisedButtons

--- src/muiTheme.js
+++ src/muiTheme.js
@@ -1,7 +1,7 @@
 import getMuiTheme from 'material-ui/styles/getMuiTheme';
 import baseTheme from 'material-ui/styles/baseThemes/darkBaseTheme';
 
-import { indigoA400, indigoA700, grey300, grey400, grey500,
+import { indigoA400, indigoA700, grey300, grey400, grey600,
   lightBlue500, lightBlue700, white, darkBlack, fullBlack } from 'material-ui/styles/colors';
 
 import {fade} from 'material-ui/utils/colorManipulator';
@@ -10,21 +10,25 @@ const muiTheme = getMuiTheme(baseTheme, {
   palette: {
     primary1Color: indigoA400,
     primary2Color: indigoA700,
-    primary3Color: grey400,
+    primary3Color: grey600,
     accent1Color: lightBlue500,
     accent2Color: lightBlue700,
-    accent3Color: grey500,
+    accent3Color: grey600,
     textColor: white,
     alternateTextColor: white,
     canvasColor: fade(fullBlack, 0.7),
     borderColor: grey300,
-    disabledColor: fade(grey400, 0.7),
+    disabledColor: fade(grey600, 0.7),
     pickerHeaderColor: lightBlue500,
     clockCircleColor: fade(darkBlack, 0.07),
     shadowColor: fade(fullBlack, 0.7)
   },
   appBar: {
     height: 56,
+  },
+  raisedButton: {
+    disabledColor: fade(indigoA400, 0.4),
+    disabledTextColor: fade(grey400, 0.4)
   },
   // eslint-disable-next-line
   userAgent: (typeof req !== 'undefined') ? req.headers['user-agent'] : false
