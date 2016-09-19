2f691d7e4f7f110d2e925b0edae32094c29e32e9
Joseph Krump <krump.joseph@gmail.com>
2016-09-19 16:28:10 -0700

    added a saga to check if a form is valid after a form field changes and dispatch an action to update the forms state

--- src/redux/sagas.js
+++ src/redux/sagas.js
@@ -1,6 +1,8 @@
 import { takeLatest, takeEvery } from 'redux-saga'
-import { call, put } from 'redux-saga/effects'
+import { call, put, select } from 'redux-saga/effects'
 import { push } from 'react-router-redux'
+
+const getForm = (state, formName) => state.forms[formName]
 
 function* redirectUserAfterLogin(action) {
    try {
@@ -13,6 +15,11 @@ function* redirectUserAfterLogin(action) {
    }
 }
 
+/**
+ * Uses react-redux-router to alter the path for a user when they logout
+ * @param {[type]} action        [description]
+ * @yield {[type]} [description]
+ */
 function* redirectUserAfterLogout(action) {
   try {
     // Clear session data
@@ -23,6 +30,11 @@ function* redirectUserAfterLogout(action) {
   }
 }
 
+/**
+ * Functions to run in response to a TOKEN_UPDATE action
+ * @param {Object} action        - The TOKEN_UPDATE action that was taken.
+ * @yield {Function}
+ */
 function* updateTokenOnUpdate(action) {
   try {
     // Clear session data
@@ -30,6 +42,37 @@ function* updateTokenOnUpdate(action) {
   } catch (e) {
      yield console.log('exception in tokenUpdatedSaga, update token', e)
   }
+}
+
+/**
+ * When an input field is updated, check to see if the form is valid also
+ * @param {Object} action        - The redux action that was taken by a saga to react to.
+ * @yield {redux-saga/effect}
+ */
+function* checkFormIsValidOnUpdate(action) {
+  let form = yield select(getForm, action.formName)
+  let isValid = yield call(checkFormIsValid, form);
+  yield put({type: 'FORM_VALID', formName: action.formName, valid: isValid})
+}
+
+/**
+ * Based on whether the fields for a form have errors or not, set the form as valid or not.
+ * @param  {Object} form - The state of the form which validation is to be checked on
+ * @return {boolean}     - returns whether or not the form is valid.
+ */
+function checkFormIsValid(form) {
+  let isValid = false;
+  Object.keys(form.fields).some((fieldName) => {
+
+    if(form.fields[fieldName].errors.length > 0) {
+      isValid = false;
+      return true;
+    } else {
+      isValid = true;
+    }
+  });
+
+  return isValid;
 }
 
 function setSessionStorage(token, user){
@@ -50,10 +93,6 @@ function clearSessionStorage(){
   }
 }
 
-    
-// redirectPath = this.props.redirectAfterLogin(push(location.state.nextPathname))
-// redirectPath = this.props.redirectAfterLogin(push('/admin'))
-
 // Generator for the admin saga
 function* loginSaga() {
   yield * takeLatest("USER_LOGGED_IN", redirectUserAfterLogin)
@@ -67,10 +106,15 @@ function* tokenUpdatedSaga() {
   yield * takeEvery("TOKEN_UPDATED", updateTokenOnUpdate)
 }
 
+function* fieldUpdateSage() {
+  yield * takeLatest("FORM_INPUT_CHANGE", checkFormIsValidOnUpdate)
+}
+
 export default function* rootSaga(){
   yield [
     loginSaga(),
     logoutSaga(),
-    tokenUpdatedSaga()
+    tokenUpdatedSaga(),
+    fieldUpdateSage()
   ]
 }
