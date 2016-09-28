943df8f31bc2766658638670bf81c5c7d3fead51
Joseph Krump <krump.joseph@gmail.com>
2016-09-27 22:48:46 -0700

    added dummy data for TreeHelper test

--- __tests__/TreeHelper.js
+++ __tests__/TreeHelper.js
@@ -1,7 +1,74 @@
 import TreeHelper from '../src/helpers/TreeHelper';
 
 const dummyTreeData = [
-
+  {
+    "id": 1,
+    "children": []
+  },
+  {
+    "id": 2,
+    "children": []
+  },
+  {
+    "id": 3,
+    "children": []
+  },
+  {
+    "id": 4,
+    "children": []
+  },
+  {
+    "id": 5,
+    "children": []
+  },
+  {
+    "id": 6,
+    "children": []
+  },
+  {
+    "id": 7,
+    "children": []
+  },
+  {
+    "id": 8,
+    "children": []
+  },
+  {
+    "id": 9,
+    "children": []
+  },
+  {
+    "id": 10,
+    "children": []
+  },
+  {
+    "id": 11,
+    "children": []
+  },
+  {
+    "id": 12,
+    "children": []
+  },
+  {
+    "id": 13,
+    "children": []
+  },
+  {
+    "id": 14,
+    "children": []
+  },
+  {
+    "id": 15,
+    "children": []
+  },
+  {
+    "id": 45,
+    "children": []
+  },
+  {
+    "id": 16,
+    "children": []
+  }
 ]
 
 const helper = new TreeHelper();
