1233c1694b875ac81b4a15c7f25d11cf7c79ff7f
Joseph Krump <krump.joseph@gmail.com>
2016-09-27 22:01:15 -0700

    ListItems switched back to a pure component

--- src/components/Pages/Admin/Resources/Index/ListItems.js
+++ src/components/Pages/Admin/Resources/Index/ListItems.js
@@ -1,34 +1,24 @@
 import React from 'react'
 import IndexItem from './IndexItem'
 
-class ListItems extends React.Component {
-  constructor(props){
-    super(props);
-    this.calcIndex = this.calcIndex.bind(this);
-  }
+const ListItems = (props) => {
+  let items = props.items.map((item, i) => {
+    return( <IndexItem 
+              key={`${props.resourceType}-${item.id}`}
+              modelId={item.id}
+              primary={item.primary}
+              secondary={item.secondary}
+              resourceType={props.resourceType}
+              deletable={item.deletable}
+              childItems={item.children}
+              depth={0}
+              root={true}
+              extraData={{...item}}
+              editMode={props.editMode}
+            />)
+  })
 
-  render () {
-    let index;
-
-    let items = this.props.items.map((item, i) => {
-
-      return( <IndexItem 
-                key={`${this.props.resourceType}-${item.id}`}
-                modelId={item.id}
-                index={index}
-                primary={item.primary}
-                secondary={item.secondary}
-                resourceType={this.props.resourceType}
-                deletable={item.deletable}
-                childItems={item.children}
-                depth={0}
-                root={true}
-                extraData={{...item}}
-                editMode={this.props.editMode}
-              />)
-    })
-    return (<div className="nested" data-parentModelId={-1}>{items}</div>)
-  }
+  return (<div className="nested" data-parentModelId={-1}>{items}</div>)
 }
 
 export default ListItems;
