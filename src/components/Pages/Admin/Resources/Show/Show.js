04e1c60717613081e1fdbf9d76bd5ea4f43add85
Joseph Krump <krump.joseph@gmail.com>
2016-10-05 13:37:32 -0700

    Resources Details component using redux store resource.name.singular value

--- src/components/Pages/Admin/Resources/Show/Show.js
+++ src/components/Pages/Admin/Resources/Show/Show.js
@@ -1,14 +1,22 @@
 import React from 'react';
 import { capitalize } from '../../../../../helpers/StringHelper'
 import AdminLayout from '../../Layout/AdminLayout'
+import {connect} from 'react-redux';
 
-const Details = ({ params: { resourceNameSingular, resourceId }, location: { query } }) => {
+const Details = (props) => {
   return (
     <AdminLayout>
       <div className="admin-index">
-        <h1>{capitalize(resourceNameSingular)} {resourceId}</h1>
+        <h1>{capitalize(props.nameSingular)} {props.params.resourceId}</h1>
       </div>
     </AdminLayout>
   );
 };
-export default Details;
+
+const mapStateToProps = (state) => {
+  return {
+    nameSingular: state.admin.resource.name.singular
+  }
+}
+
+export default connect(mapStateToProps)(Details);
