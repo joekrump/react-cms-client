4eb28c48c9a70976105bdb78edeaf6bfddcba443
Joseph Krump <krump.joseph@gmail.com>
2016-10-04 22:33:01 -0700

    styling updated to allow for TopNav to display in Page Edit

--- src/components/Pages/Admin/Layout/EditPageLayout.js
+++ src/components/Pages/Admin/Layout/EditPageLayout.js
@@ -2,9 +2,10 @@ import React from 'react'
 import SpeedDial from '../../../Menu/SpeedDial'
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
 import s from './EditPageLayout.scss'
-
+import TopNav from '../../../Nav/SiteTopNav'
 const EditPageLayout = (props) => (
   <div className="edit-page-container">
+    <TopNav />
     {props.children}
     <SpeedDial />
   </div>
