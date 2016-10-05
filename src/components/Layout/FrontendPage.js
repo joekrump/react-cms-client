d86b990d6653b6bdad91fec7a1e321d01da428a2
Joseph Krump <krump.joseph@gmail.com>
2016-10-04 22:42:43 -0700

    styling updated for pages where user is logged in but view frontend

--- src/components/Layout/FrontendPage.js
+++ src/components/Layout/FrontendPage.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import s from './FrontendPage.scss'
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import TopNav from '../Nav/SiteTopNav'
+import {connect} from 'react-redux'
 
 const FrontendPage = (props) => (
-  <div className="frontend-page-container">
+  <div className={"frontend-page-container" + (props.loggedIn ? ' logged-in' : '')}>
+    {props.loggedIn ? <TopNav /> : null}
     {props.children}
   </div>
 ) 
 
-export default withStyles(s)(FrontendPage);
+const mapStateToProps = (state) => {
+  return {
+    loggedIn: state.auth.logged_in
+  }
+}
+
+export default connect(mapStateToProps)(withStyles(s)(FrontendPage));
