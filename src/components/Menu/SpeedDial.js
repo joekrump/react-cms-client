4f6a718cb023702dd4caf72e94fd4f20a29eb156
Joseph Krump <krump.joseph@gmail.com>
2016-10-11 17:02:52 -0700

    SpeedDial icon for add notes updated

--- src/components/Menu/SpeedDial.js
+++ src/components/Menu/SpeedDial.js
@@ -6,6 +6,7 @@ import AddPageIcon from 'material-ui/svg-icons/action/note-add'
 import UserIcon from 'material-ui/svg-icons/social/person-add'
 import BookIcon from 'material-ui/svg-icons/av/library-books'
 import RoleIcon from 'material-ui/svg-icons/social/group-add'
+import AddNoteIcon from 'material-ui/svg-icons/image/add-to-photos'
 import PermissionIcon from 'material-ui/svg-icons/hardware/security'
 import { push } from 'react-router-redux'
 import withStyles from 'isomorphic-style-loader/lib/withStyles';
@@ -13,7 +14,7 @@ import s from './SpeedDial.scss'
 
 const actions = [
   {icon: <AddPageIcon />, route: '/admin/pages/new', tooltipText: 'Create a new Page'},
-  {icon: <AddPageIcon />, route: '/admin/cards/new', tooltipText: 'Create a new Card'},
+  {icon: <AddNoteIcon />, route: '/admin/cards/new', tooltipText: 'Create a new Card'},
   {icon: <UserIcon />, route: '/admin/users/new', tooltipText: 'Create a new User'},
   {icon: <BookIcon />, route: '/admin/books/new', tooltipText: 'Create a new Book'},
   {icon: <RoleIcon />, route: '/admin/roles/new', tooltipText: 'Create a new User Role'},
