a3ce162d8855e186f19cf2124a3f89b77493ae44
Joseph Krump <krump.joseph@gmail.com>
2016-10-12 12:08:10 -0700

    beginning to build drag functionality for cards

--- src/components/Pages/Card/Card.js
+++ src/components/Pages/Card/Card.js
@@ -5,25 +5,64 @@ import withStyles from 'isomorphic-style-loader/lib/withStyles';
 
 class Card extends React.Component {
 
-  onStart() {
+  constructor(props) {
+    super(props);
+    this.state = {
+      target: null,
+      startX: 0,
+      currentX: 0
+    }
+    requestAnimationFrame(this.update);
+  }
+  componentDidMount() {
+    this.addEventListeners()
+  }
+
+  componentWillUnmount() {
 
   }
 
-  onMove() {
+  addEventListeners() {
+    document.addEventListener('touchstart', (evt) => this.onStart(evt))
+    document.addEventListener('touchmove', (evt) => this.onMove(evt))
+    document.addEventListener('touchend', (evt) => this.onEnd(evt))
+  }
+
+  removeEventListeners() {
 
   }
 
-  onEnd() {
+  onStart(evt) {
+    if(!evt.target.classList.contains('card')) {
+      return;
+    }
+    let startX = evt.pageX || evt.touches[0].pageX;
 
+    this.setState({
+      target: evt.target,
+      startX: startX,
+      currentX: startX
+    })
+    evt.preventDefault();
   }
 
-  update() {
+  onMove(evt) {
+    if(!this.state.target)
+      return;
+  }
+
+  onEnd(evt) {
+    if(!this.state.target)
+      return;
+  }
+
+  update(evt) {
 
   }
 
   render() {
     return(
-      <div className="card">
+      <div className="card" ref="card">
         {this.props.children}
       </div>
     )
