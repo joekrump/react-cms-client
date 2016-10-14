c93556b68b958b6cee970fec0a85de5cdc6438c6
Joseph Krump <krump.joseph@gmail.com>
2016-10-14 14:26:31 -0700

    added EditDrawer and its styles

---
+++ src/components/Menu/EditDrawer.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import withStyles from 'isomorphic-style-loader/lib/withStyles';
+import s from './EditDrawer.scss';
+import Drawer from 'material-ui/Drawer';
+import AppBar from 'material-ui/AppBar';
+import RaisedButton from 'material-ui/RaisedButton';
+
+class EditDrawer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {open: false};
+  }
+
+  handleToggle = () => this.setState({open: !this.state.open});
+
+  render() {
+    return (
+      <div>
+        <RaisedButton
+          className="page-menu floating-menu"
+          label="Toggle Drawer"
+          onTouchTap={this.handleToggle}
+        />
+        <Drawer width={346} openSecondary={true} open={this.state.open}>
+          <AppBar title="AppBar" />
+          {this.props.children}
+        </Drawer>
+      </div>
+    );
+  }
+}
+
+export default withStyles(s)(EditDrawer);
+
+
+
