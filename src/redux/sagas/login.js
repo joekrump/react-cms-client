745cc88e8f89fa43058da36192c02f94d4f48c92
Joseph Krump <krump.joseph@gmail.com>
2016-10-16 01:01:00 -0700

    removed console logs in login saga

--- src/redux/sagas/login.js
+++ src/redux/sagas/login.js
@@ -3,7 +3,6 @@ import { call, put } from 'redux-saga/effects'
 import { push } from 'react-router-redux'
 
 function setSessionStorage(token, user){
-  console.log('SET SESSION STORAGE')
   if(typeof sessionStorage !== 'undefined'){
     if(token) {
       sessionStorage.laravelAccessToken = token;
@@ -16,7 +15,6 @@ function setSessionStorage(token, user){
 }
 
 function* redirectUserAfterLogin(action) {
-  console.log('redirectUserAfterLogin');
    try {
       yield call(setSessionStorage, action.token, action.user);
       if(action.redirectPath !== undefined) {
