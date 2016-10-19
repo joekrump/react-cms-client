1275b7a459ac0693b0e5e5d46f02b6214f406d8f
Joseph Krump <krump.joseph@gmail.com>
2016-10-19 11:17:49 -0700

    removed location saga

--- src/redux/sagas/root.js
+++ src/redux/sagas/root.js
@@ -4,12 +4,10 @@ import tokenSaga      from './tokenUpdated';
 import formFieldSaga  from './fieldUpdate';
 import adminIndexSaga from './ui/admin/index';
 import adminSaga      from './admin';
-import locationSaga   from './location';
 
 export default function* rootSaga(){
   yield [
     loginSaga(),
-    locationSaga(),
     adminSaga(),
     adminIndexSaga(),
     formFieldSaga(),
