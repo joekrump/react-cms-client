5a7533f9e0bf45afb81893665e8ccd7ff430c6db
Joseph Krump <krump.joseph@gmail.com>
2016-11-04 13:04:27 -0700

    added file containing imports for all intial states

---
+++ src/redux/store/initial_states/index.js
@@ -0,0 +1,17 @@
+import adminState from '../store/initial_states/admin/admin';
+import authState from '../store/initial_states/auth';
+import formsState from '../store/initial_states/forms/forms';
+import notificationState from '../store/initial_states/notification';
+import pageState from '../store/initial_states/page';
+import paymentState from '../store/initial_states/payment';
+import treeState from '../store/initial_states/tree';
+
+export default {
+  adminState,
+  authState,
+  formsState,
+  notificationState,
+  pageState,
+  paymentState,
+  treeState
+}
