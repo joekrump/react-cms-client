a318f08824eae023895bf8b28f5c0690da569b16
Joseph Krump <krump.joseph@gmail.com>
2016-11-19 16:34:45 -0800

    cleaned up some of the code in social links

--- src/components/Pages/Page/SocialLinks.js
+++ src/components/Pages/Page/SocialLinks.js
@@ -1,23 +1,36 @@
 import React from 'react';
 import TwitterShare from '../../Social/TwitterShare';
 import FacebookShare from '../../Social/FacebookShare';
+import MailShare from '../../Social/MailShare';
+import PintrestShare from '../../Social/PintrestShare';
+import LinkedInShare from '../../Social/LinkedInShare';
+import GoogleShare from '../../Social/GoogleShare';
+import PrintPage from '../../Social/Print';
 
 class SocialLinks extends React.PureComponent {
-  print(evt) {
-    evt.preventDefault();
 
-    if(typeof(window) !== 'indefined') {
-      window.print();
-    }
-  }
   render() {
     return (
       <div id="story-social">
-        <TwitterShare url={this.props.url}/>
+        <TwitterShare url={this.props.url} />
+        <FacebookShare url={this.props.url} />
+        <MailShare 
+          subject={this.props.title} 
+          body={this.props.summary} 
+          url={this.props.url} 
+        />
+        <PintrestShare url={this.props.url} 
+          
 
-        <FacebookShare url={this.props.url}/>
-
-        <a href="http://www.printfriendly.com" onClick={this.print} className="svgprint" title="Print this story"><svg width="32px" height="32px" viewBox="0 0 32 32"><desc>Print</desc><path d="M26.238,9.174c-0.513,0-1.194,0-1.707,0V2.349H7.468v6.825c-3.925,0-6.826,0-6.826,0v10.238 c0,2.902,2.218,5.119,5.119,5.119h1.707v5.119h17.063v-5.119h6.826V14.293C31.357,11.393,29.14,9.174,26.238,9.174z M9.174,4.055 h13.652v5.119c-4.267,0-9.386,0-13.652,0V4.055z M22.826,27.945H9.174V17.707h10.238h3.414V27.945z"></path></svg></a>
+        />
+        <LinkedInShare url={this.props.url}
+          title={this.props.title}
+          summary={this.props.summary}
+          soruce={this.props.baseUrl}
+        />
+        <GoogleShare url={this.props.url}/>
+        <PrintPage />
+        
       </div>
     );
   }
