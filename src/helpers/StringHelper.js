b6e2e76089bb15eaf3e4547a3a29742792b76559
Joseph Krump <krump.joseph@gmail.com>
2016-12-27 17:36:06 -0800

    removed unnecessary escape characters

--- src/helpers/StringHelper.js
+++ src/helpers/StringHelper.js
@@ -11,7 +11,7 @@ export function slugify(text)
   return text.toString().toLowerCase()
     .replace(/-$/, '')
     .replace(/\s+/g, '-')           // Replace spaces with -
-    .replace(/[^\w\-]+/g, '')       // Remove all non-word chars
-    .replace(/\-\-+/g, '-')         // Replace multiple - with single -
+    .replace(/[^\w-]+/g, '')       // Remove all non-word chars
+    .replace(/--+/g, '-')         // Replace multiple - with single -
     .replace(/^-+/, '')
 }
