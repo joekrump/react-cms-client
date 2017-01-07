96d54a85a4e8f318040e499292d69050bd2a29d3
Joseph Krump <krump.joseph@gmail.com>
2017-01-06 20:46:53 -0800

    correct draft state showing in DraftLabel

--- src/components/Pages/Admin/Resources/Index/DraftLabel.js
+++ src/components/Pages/Admin/Resources/Index/DraftLabel.js
@@ -13,30 +13,21 @@ export default class Label extends React.Component {
 
   constructor(props) {
     super(props);
-
-    this.state = {
-      draft: this.props.draft
-    }
   }
 
   toggleDraftStatus() {
-    this.setState({
-      draft: !this.state.draft
-    })
     // Todo: toggle the draft value for this item.
   }
-
-
 
   render() {
     return (
       <Chip
-        backgroundColor={this.state.draft ? indigo300 : indigo900}
+        backgroundColor={this.props.isDraft ? indigo300 : indigo900}
         labelColor="white"
         onTouchTap={() => this.toggleDraftStatus()}
         style={styles.chip}
       >
-        {this.state.draft ? 'Draft' : 'Published'}
+        {this.props.isDraft ? 'Draft' : 'Published'}
       </Chip>
     );
   }
